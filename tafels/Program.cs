0d479973ef69a1b5b74e4b7fe618947980ff27ca
Mats Bos <mats.bos@immalle.be>
2018-02-05 09:23:14 +0100

    10 tafels toegevoegd + het weergeven van de width en height van de tafels

--- tafels/Program.cs
+++ tafels/Program.cs
@@ -10,6 +10,26 @@ namespace tafels
     {
         static void Main(string[] args)
         {
+           
+            var tafels = new List<Table>();
+            var rndGen = new Random();
+
+            for(var i = 0; i < 10; i++)
+            {
+                var a = rndGen.Next(50, 200);
+                var b = rndGen.Next(50, 200);
+
+                tafels.Add(new Table(a, b));
+            }
+
+            foreach (var t in tafels)
+            { 
+                t.ShowData();
+                
+            }
+
         }
+
+        
     }
 }
---
+++ tafels/table.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tafels
+{
+    class Table
+    {
+
+        private int height;
+        private int width;
+
+
+        public Table(int height,int width)
+        {
+            this.height = height;
+            this.width = width;
+        }
+
+
+        public void ShowData()
+        {
+            Console.WriteLine("de hoogte is {0} en de breedte is {1}",height,width);
+
+        }
+
+
+
+    }
+}
